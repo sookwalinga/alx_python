640f45b14f7fe2a8492545cc1e527ac3fad04332
sam <sookwalinga@gmail.com>
2023-07-18 22:25:53 +0300

    refactor: task3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,7 +4,6 @@ number = random.randint(-10000, 10000)
 
 last_digit = number % 10
 
-
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
