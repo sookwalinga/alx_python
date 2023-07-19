431c885e9b2eec850ecfc030f67136f1f1a5d072
sam <sookwalinga@gmail.com>
2023-07-19 20:38:58 +0300

    feat: create 4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+
+    sequence = [0, 1]
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence
