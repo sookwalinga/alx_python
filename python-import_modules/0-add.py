ea7449350a92ec5b9cdff17ca20b919a2eeabbbf
sam <sookwalinga@gmail.com>
2023-07-25 16:08:08 +0300

    feat: create 0-add.py

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b
+
+
+result = add(a, b)
+print(f"{a} + {b} = {result}")
