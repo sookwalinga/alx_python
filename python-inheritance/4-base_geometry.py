7d41ab81763d17d4e74a4785ecb10b91e93ad1f6
sam <sookwalinga@gmail.com>
2023-08-02 22:28:19 +0300

    feat: create 4-base_geometry.py

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,22 @@
+"""
+Module: 4-base_geometry
+
+Contains a class definition for BaseGeometry
+with a public instance method.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing the base geometry.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+
+        """
+        raise Exception("area() is not implemented")
