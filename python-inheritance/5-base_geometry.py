c049ca76625d57e60f2a7045f025c9f344fcff25
sam <sookwalinga@gmail.com>
2023-08-02 23:07:35 +0300

    refactor

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,60 +6,26 @@ This module contains the BaseGeometry class.
 
 class BaseGeometry:
     """
-    A class representing basic geometry operations and validation.
-
-    Attributes:
-        None
-
-    Methods:
-        area(): Raises an Exception with the message "area() is not implemented."
-        integer_validator(name, value): Validates if a given value is a positive integer.
+    A class named BaseGeometry.
     """
 
     def area(self):
         """
-        Calculate the area of the geometry. This method is not implemented in the base class.
-
-        Raises:
-            Exception: Always raises an Exception with the message "area() is not implemented."
+        Placeholder method for calculating the area.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validate if a given value is a positive integer.
-
+        Validates that value is a positive integer.
         Args:
-            name (str): The name of the value being validated.
+            name (str): The name of the value.
             value (int): The value to be validated.
-
         Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is not a positive integer.
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-
-# Example usage
-bg = BaseGeometry()
-
-bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
-
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
