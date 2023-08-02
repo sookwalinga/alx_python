60d446db50d95f8aa5dea2b9b0554c3b7c5e6925
sam <sookwalinga@gmail.com>
2023-08-02 23:04:27 +0300

    refactor

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,26 +6,57 @@ This module contains the BaseGeometry class.
 
 class BaseGeometry:
     """
-    A class named BaseGeometry.
+    A base class representing geometric shapes.
+
+    This class defines methods for calculating the area of geometric shapes
+    and validating integer values for certain properties.
     """
 
     def area(self):
         """
-        Placeholder method for calculating the area.
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validates that value is a positive integer.
+        Validate an integer value for a specific property.
+
         Args:
-            name (str): The name of the value.
+            name (str): The name of the property being validated.
             value (int): The value to be validated.
+
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is not a positive integer.
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+# Example usage
+bg = BaseGeometry()
+
+# Validating integer values
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
