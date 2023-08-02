be5691f80c1d04d0a161c701c22f350de689bece
sam <sookwalinga@gmail.com>
2023-08-02 22:15:14 +0300

    feat: create 3-base_geometry.py

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,12 @@
+"""
+Module: 3-base_geometry
+
+Contains an empty class definition for BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing the base geometry.
+    """
+    pass
