ec1bb36a62b96c9af91a7c69293a0e58bdcc01ce
sam <sookwalinga@gmail.com>
2023-08-09 20:00:11 +0000

    9 Aug 2023, 23:00

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
-"""
-import sys
-import urllib.request
+"""Find the value of the X-Request-Id in response"""
+
+from urllib import request, error
+from sys import argv
+
+
+def request_header_property(url: str) -> str:
+    """
+    Send a request to the URL specified and
+    get the response headers
+    Args:
+        url (str): The URL to query
+    """
+    try:
+        with request.urlopen(url) as response:
+            return response.info()['X-Request-Id']
+    except error.URLError as e:
+        return e.reason
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    print(request_header_property(argv[1]))
