2b7bc3957cc5b55ac0008b74351bb2f19a12b785
sam <sookwalinga@gmail.com>
2023-08-09 20:13:02 +0000

    9 Aug 2023, 23:13

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -16,24 +16,13 @@ def get_x_request_id(url):
     Args:
         url (str): The URL to send the request to.
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
+    response = requests.get(url)
 
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    if 'X-Request-Id' in response.headers:
+        x_request_id = response.headers['X-Request-Id']
+        print(x_request_id)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
     get_x_request_id(url)
