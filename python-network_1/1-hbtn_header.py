5a693b6a7bf4aea3c49cea879b7c27743f0e4635
sam <sookwalinga@gmail.com>
2023-08-09 22:47:51 +0300

    test: task2

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,26 +2,32 @@
 """
     Takes a URL as input, sends a request, and displays the value of the X-Request-Id header.
 """
-
 import requests
 import sys
 
 
-def fetch_and_display_request_id(url):
+def get_x_request_id(url):
     """
-    Fetches the provided URL, displays the value of X-Request-Id header in response.
+    Sends a request to the provided URL and retrieves the value of the X-Request-Id header.
+
+    Args:
+        url (str): The URL to send the request to.
+
+    Returns:
+        str: The value of the X-Request-Id header in the response.
     """
     response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-
-    if request_id:
-        print(request_id)
+    x_request_id = response.headers.get('X-Request-Id')
+    return x_request_id
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url = sys.argv[1]
-    fetch_and_display_request_id(url)
+        print("Usage: python script_name.py <URL>")
+    else:
+        url = sys.argv[1]
+        x_request_id = get_x_request_id(url)
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id header not found in the response.")
