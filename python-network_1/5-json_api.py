c728d509e32071611a16a7f45c89a700b49a4442
sam <sookwalinga@gmail.com>
2023-08-09 23:29:11 +0300

    doc: task5

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,8 +1,10 @@
-import sys
-import requests
 #!/usr/bin/env python3
 """Sends a POST request to http: // 0.0.0.0: 5000/search_user with a given letter.
-Usage: ./8-json_api.py < letter >
+"""
+import sys
+import requests
+
+"""Usage: ./8-json_api.py < letter >
 - The letter is sent as the value of the variable `q`.
 - If no letter is provided, sends `q = ""`.
 """
