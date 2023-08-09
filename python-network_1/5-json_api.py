252bede28833a673fb6edaf5dfe4bb2d21832af0
sam <sookwalinga@gmail.com>
2023-08-09 23:20:34 +0300

    chore: handle task5

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,23 +1,45 @@
-#!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+#!/usr/bin/env python3
 """
-import sys
+Module to send a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter.
+"""
+
 import requests
+import sys
+
+
+def search_user(letter):
+    """
+    Send a POST request to http://0.0.0.0:5000/search_user with the provided letter as a parameter.
+
+    Args:
+        letter (str): The letter to be used as the 'q' parameter in the request.
+    """
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': letter}
+
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
+
+        try:
+            json_response = response.json()
+            if json_response:
+                user_id = json_response.get('id')
+                user_name = json_response.get('name')
+                print("[{}] {}".format(user_id, user_name))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    if len(sys.argv) != 2:
+        print("Usage: {} <letter>".format(sys.argv[0]))
+        sys.exit(1)
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
+    letter = sys.argv[1]
+    search_user(letter)
