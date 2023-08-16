94ff3576fd0b77ce074476d6081471c4f6270cae
sam <sookwalinga@gmail.com>
2023-08-16 15:28:35 +0300

    refactor

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,21 +7,26 @@ import requests
 import sys
 
 
-def post_email(url, email):
+def main():
     """
     Sends a POST request with the provided email to the given URL and displays the response body.
     """
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-
-    print(response.text)
-
-
-if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        print("Usage: python script.py <URL> <email>")
         sys.exit(1)
 
     url = sys.argv[1]
     email = sys.argv[2]
-    post_email(url, email)
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    if response.status_code == 200:
+        print("Response body:")
+        print(response.text)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
