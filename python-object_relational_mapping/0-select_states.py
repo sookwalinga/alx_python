43bdd61164fa7995eadace9cc9c24fc29f14dc27
sam <sookwalinga@gmail.com>
2023-08-23 10:35:01 +0000

    23 Aug 2023, 13:35

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Script to list states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+# Check if the correct number of arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    sys.exit(1)
+
+# Retrieve command line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+try:
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    # Create a cursor to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SELECT query
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+except MySQLdb.Error as e:
+    print("MySQL Error: {}".format(e))
+    sys.exit(1)
