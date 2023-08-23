99d831036cf43bee76b6ce926aaf42da993b99b1
sam <sookwalinga@gmail.com>
2023-08-23 14:40:56 +0300

    refactor

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,8 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-    
     """State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
