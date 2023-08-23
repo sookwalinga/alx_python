209ac9435716076a96163a161204d6b2ddf9e97e
sam <sookwalinga@gmail.com>
2023-08-23 14:45:04 +0300

    fix: remove whitespaces from blank lines

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
         print("Usage: {} <username> <password> <database>"
               .format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, database),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states_with_a = session.query(
         State).filter(State.name.like('%a%')).order_by(State.id).all()
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
-    
+
     session.close()
