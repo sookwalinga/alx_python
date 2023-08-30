2a5f08b56429f6693f7933f40326e94bc97bcad3
sam <sookwalinga@gmail.com>
2023-08-30 20:20:39 +0000

    30 Aug 2023, 23:20

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f'C {text.replace("_", " ")}'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
