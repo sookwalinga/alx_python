7c0c5e309b5ca050a2934db2b1d1db1cb9f8d6f4
sam <sookwalinga@gmail.com>
2023-08-30 20:33:46 +0000

    30 Aug 2023, 23:33

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,4 +1,3 @@
-# Script that starts a Flask web application
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,4 +12,3 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
