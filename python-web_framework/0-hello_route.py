98ebc9475ce6398d871efc1f6b5ebce93990810f
sam <sookwalinga@gmail.com>
2023-08-30 20:30:01 +0000

    30 Aug 2023, 23:30

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,21 @@
+# Import the Flask class from the flask module
 from flask import Flask
 
+# Create an instance of the Flask class and use '__name__' as the module name
 app = Flask(__name__)
 
+# Decorator to define a route for the root URL ('/') and disable strict trailing slashes
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    Define the behavior when the root URL is accessed.
+
+    Returns:
+        str: A greeting message "Hello HBNB!".
+    """
     return 'Hello HBNB!'
 
+# Check if the script is being run directly (not imported as a module)
 if __name__ == '__main__':
+    # Run the Flask app
     app.run(host='0.0.0.0', port=5000)
