0b9920d342bf3cb69caa900c6e4743cc7453b417
sam <sookwalinga@gmail.com>
2023-08-30 20:21:35 +0000

    30 Aug 2023, 23:21

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f'C {text.replace("_", " ")}'
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return f'Python {text.replace("_", " ")}'
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
