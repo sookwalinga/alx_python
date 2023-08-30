879733b8c4fdafcd475517e1777a05620518f8a2
sam <sookwalinga@gmail.com>
2023-08-30 23:39:41 +0300

    refactor

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,3 +1,8 @@
+"""
+This module implements a Flask web application with
+various routes and template rendering.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -30,7 +35,8 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     even_odd = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', number=n, even_odd=even_odd)
+    return render_template('6-number_odd_or_even.html',
+                           number=n, even_odd=even_odd)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
