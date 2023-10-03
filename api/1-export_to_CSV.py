f3fa2aa55b45cf5b0906c6f8564e30b85a98d36c
sam <sookwalinga@gmail.com>
2023-10-03 20:10:51 +0300

    test: task1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,6 @@
-import csv
-import os
 import requests
 import sys
+import csv
 
 def get_employee_info(employee_id):
     # Define the API endpoints
@@ -11,36 +10,35 @@ def get_employee_info(employee_id):
     # Fetch employee information
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    user_id = user_data.get('id')
-    username = user_data.get('username')
+    employee_id = user_data.get('id')
+    employee_name = user_data.get('username')
 
     # Fetch employee's TODO list
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-    # Create a CSV file with the user ID as the name
-    csv_filename = f'{user_id}.csv'
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
+    # Create a CSV file to save the tasks
+    csv_filename = f'{employee_id}.csv'
+
+    # Write tasks to CSV
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        # Write tasks to the CSV file
         for todo in todos_data:
-            task_completed_status = "True" if todo['completed'] else "False"
-            csv_writer.writerow([user_id, username, task_completed_status, todo['title']])
+            task_completed_status = "Completed" if todo['completed'] else "Not Completed"
+            csv_writer.writerow([employee_id, employee_name, task_completed_status, todo['title']])
 
-    print(f"Data exported to {csv_filename}")
-
-    # Reopen the CSV file for reading and count the number of tasks
-    with open(csv_filename, 'r') as f:
-        num_tasks = sum(1 for _ in f) - 1  # Subtract 1 to exclude the header row
-
-    print(f"Number of tasks in CSV: {num_tasks} OK")
+    print(f"Data has been exported to {csv_filename}.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 export_to_CSV.py <employee_id>")
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_info(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
