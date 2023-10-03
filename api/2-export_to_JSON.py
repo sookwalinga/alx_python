45566f3b896f03e32e8d67bd64f4f7e83c5e0fe2
sam <sookwalinga@gmail.com>
2023-10-03 20:36:38 +0300

    refactor

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -10,33 +10,40 @@ def get_employee_info(employee_id):
     # Fetch employee information
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    user_id = user_data.get('id')
-    username = user_data.get('username')
+    employee_id = user_data.get('id')
+    employee_name = user_data.get('username')
 
     # Fetch employee's TODO list
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-    # Create a JSON object with the required format
-    employee_tasks = {
-        "USER_ID": [{
-            "task": todo['title'],
-            "completed": todo['completed'],
-            "username": username
-        } for todo in todos_data]
+    # Create a JSON data structure
+    employee_json_data = {
+        "USER_ID": [
+            {
+                "task": todo['title'],
+                "completed": todo['completed'],
+                "username": employee_name
+            }
+            for todo in todos_data
+        ]
     }
 
-    # Create a JSON file with the user ID as the name
-    json_filename = f'{user_id}.json'
+    # Write JSON data to a file
+    json_filename = f'{employee_id}.json'
     with open(json_filename, 'w') as json_file:
-        json.dump(employee_tasks, json_file, indent=4)
+        json.dump(employee_json_data, json_file, indent=4)
 
-    print(f"Data exported to {json_filename}")
+    print(f"Data has been exported to {json_filename}.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 export_to_JSON.py <employee_id>")
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_info(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
